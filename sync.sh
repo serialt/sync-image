9270522c9c1dface97264b7ba8a501652598935f
Serialt <serialt.tang@gmail.com>
2021-11-15 15:50:19 +0800

    feat: add sync to aliyun hub

--- sync.sh
+++ sync.sh
@@ -3,6 +3,9 @@
 hub="docker.io"
 repo="$hub/serialt"
 
+hub2="registry.cn-hangzhou.aliyuncs.com"
+repo2="$hub2/serialt"
+
 
 
 if [ -f sync.yaml ]; then
@@ -12,6 +15,9 @@ if [ -f sync.yaml ]; then
     && sudo skopeo --insecure-policy sync --src yaml --dest docker sync.yaml ${repo} \
     && sudo skopeo --insecure-policy sync --src yaml --dest docker custom_sync.yaml ${repo}
 
+    sudo skopeo login -u ${HUB_USERNAME} -p ${HUB_PASSWORD} ${hub2} \
+    && sudo skopeo --insecure-policy sync --src yaml --dest docker sync.yaml ${repo2} \
+    && sudo skopeo --insecure-policy sync --src yaml --dest docker custom_sync.yaml ${repo2}
 
 
    echo "[End] done."
