0d43dcbdaf876c24dde46c4b1170233b38633b1e
serialt <t@imau.cc>
2023-02-25 12:14:11 +0800

    fix: env error

--- sync.sh
+++ sync.sh
@@ -1,21 +1,21 @@
 #!/bin/bash
 
 hub="docker.io"
-repo="$hub/serialt"
+repo="$hub/${DEST_HUB_USERNAME}"
 
 hub2="registry.cn-hangzhou.aliyuncs.com"
-repo2="$hub2/serialt"
+repo2="$hub2/${DEST_HUB_USERNAME}"
 
 
 
 if [ -f sync.yaml ]; then
    echo "[Start] sync......."
    
-    sudo skopeo login -u ${HUB_USERNAME} -p ${HUB_PASSWORD} ${hub} \
+    sudo skopeo login -u ${DEST_HUB_USERNAME} -p ${DEST_HUB_PASSWORD} ${hub} \
     && sudo skopeo --insecure-policy sync -a --src yaml --dest docker sync.yaml ${repo} \
     && sudo skopeo --insecure-policy sync -a --src yaml --dest docker custom_sync.yaml ${repo}
     sleep 3
-    sudo skopeo login -u ${HUB_USERNAME} -p ${HUB_PASSWORD} ${hub2} \
+    sudo skopeo login -u ${DEST_HUB_USERNAME} -p ${DEST_HUB_PASSWORD} ${hub2} \
     && sudo skopeo --insecure-policy sync -a --src yaml --dest docker sync.yaml ${repo2} \
     && sudo skopeo --insecure-policy sync -a  --src yaml --dest docker custom_sync.yaml ${repo2}
 
